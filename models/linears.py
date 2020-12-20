cc4e8732bbec57ef4059d8f17429a3e1e72379b1
Tong Ge <comword@live.com>
2020-12-20 15:43:20 +0000

    learn on all data groups

--- models/linears.py
+++ models/linears.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import GridSearchCV
 class LinRegWithPoly(BaseModel):
     # hyper-paramters:
     # degree: the degree of the polynomial
-    # degree_range: the auto search range of the degree of the polynomial (default: np.arange(1, 6))
+    # degree_range: the auto search range of the degree of the polynomial (default: np.arange(1, 3))
     # if degree is set, the degree_range will be omitted
     # kfold: folds number (default=5)
 
@@ -19,9 +19,9 @@ class LinRegWithPoly(BaseModel):
     def __init__(self, *model_params, **configs):
         self.configs = configs
 
-        def get_pipeline(degree=5, **kwargs):
+        def get_pipeline(degree=3, **kwargs):
             return Pipeline([('poly', PolynomialFeatures(degree)),
-                             ('reg', LinearRegression(**kwargs))])
+                             ('model', LinearRegression(**kwargs))])
 
         self.initSKModel(get_pipeline, model_params, configs)
 
@@ -30,7 +30,7 @@ class LinRegWithPoly(BaseModel):
         if "degree" not in self.configs:
             # grid search
             self.hyper_parm_grid.append({
-                "poly__degree": self.configs.get("degree_range", np.arange(1, 6))
+                "poly__degree": self.configs.get("degree_range", np.arange(1, 3))
                 })
             self.model = GridSearchCV(self.model, self.hyper_parm_grid,
                 cv=self.configs.get("kfold", 5), scoring='neg_mean_squared_error')
@@ -41,25 +41,21 @@ class LinRegWithPoly(BaseModel):
         for scorer in ["neg_mean_squared_error",
                        "neg_root_mean_squared_error", "r2"]:
             self.metrics["val_"+scorer] = SCORERS[scorer](self.model, X_test, y_test)
-        return self.metrics["val_neg_mean_squared_error"], self.metrics
+        return self.metrics["val_r2"], self.metrics
 
 from sklearn.linear_model import RidgeCV, LassoCV
 class RidgeRegCVWithPoly(LinRegWithPoly):
     def __init__(self, *model_params, **configs):
         super().__init__(*model_params, **configs)
-        def get_pipeline(degree=5, **kwargs):
+        def get_pipeline(degree=3, alphas=[1e-2, 1e-1, 1], **kwargs):
             return Pipeline([('poly', PolynomialFeatures(degree)),
-                             ('reg', RidgeCV(**kwargs))])
+                             ('model', RidgeCV(alphas=alphas, **kwargs))])
         self.initSKModel(get_pipeline, model_params, configs)
 
 class LassoCVWithPoly(LinRegWithPoly):
     def __init__(self, *model_params, **configs):
         super().__init__(*model_params, **configs)
-        def get_pipeline(degree=5, **kwargs):
+        def get_pipeline(degree=3, alphas=np.logspace(-2, 1, 10), **kwargs):
             return Pipeline([('poly', PolynomialFeatures(degree)),
-                             ('reg', LassoCV(**kwargs))])
+                             ('model', LassoCV(alphas=alphas, **kwargs))])
         self.initSKModel(get_pipeline, model_params, configs)
-
-if __name__ == "__main__":
-    lasso = LassoCVWithPoly(degree=3)
-    print(lasso.named_steps['poly'].degree)
